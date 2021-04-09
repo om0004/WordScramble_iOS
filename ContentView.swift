9ce437cbf152c40689cf3f4c4240c1cf6d6df644
om <60003596+om0004@users.noreply.github.com>
2021-04-09 18:38:01 +0530

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -2,104 +2,136 @@
 //  ContentView.swift
 //  WordScramble
 //
-//  Created by om on 08/02/21.
+//  Created by om on 07/04/21.
 //
 
 import SwiftUI
-
 struct ContentView: View
 {
+    
     @State private var usedWords=[String]()
     @State private var rootWord=""
     @State private var newWord=""
-    @State private var errorTitle = ""
-    @State private var errorMessage = ""
-    @State private var showingError = false
-    var body: some View
-    {
-        NavigationView
-        {
-            VStack()
-            {
-                TextField("Enter your word",text:$newWord,onCommit:addNewWord)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .padding()
-                    .autocapitalization(.none)
-                List(usedWords,id:\.self)
-                {
-                    Image(systemName:"\($0.count).circle")
-                    Text($0)
-                }
-            }
-            .navigationBarTitle(rootWord)
-        }
-        .onAppear(perform:startGame)
-        .alert(isPresented:$showingError)
-        {
-            Alert(title: Text(errorTitle), message: Text(errorMessage), dismissButton: .default(Text("OK")))
-        }
-    }
+    @State private var showingError=false
+    @State private var errorTitle=""
+    @State private var errorMessage=""
+    @State private var score=0
     func addNewWord()
     {
-        let word=newWord.lowercased().trimmingCharacters(in:. whitespacesAndNewlines)
-        guard word.count>0 else {return}
-        guard isOriginal(word:word) else {
-            wordError(title: "Word used already", message: "Be more original")
-            return
-        }
-        guard isPossible(word:word) else {
-            wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
-            return
-        }
-        guard isReal(word:word) else {
-            wordError(title: "Word not possible", message: "That isn't a real word.")
-            return
-        }
-        usedWords.insert(word,at:0)
-        newWord=""
-    }
-    func startGame()
-    {
-        if let startWordUrl=Bundle.main.url(forResource:"start", withExtension:"txt")
+        let answer = newWord.lowercased().trimmingCharacters(in:.whitespacesAndNewlines)
+        guard answer.count>1 else{return}
+        guard isOriginal(word:answer) else
         {
-            if let startWord=try? String(contentsOf:startWordUrl)
-            {
-                let allWords=startWord.components(separatedBy:"\n")
-                rootWord=allWords.randomElement() ?? "silkworm"
-                return
-            }
+            wordError(title:"Word used originally", message:"Be more original")
+            score=score-1
+            return
         }
-        fatalError("could not load file")
+        guard isPossible(word:answer) else
+        {
+            wordError(title:"word not recognized", message:"You cant just make them up")
+            score=score-2
+            return
+        }
+        guard isReal(word: answer) else
+        {
+            wordError(title: "Word not possible", message: "That isn't a real word.")
+            score=score-3
+            return
+        }
+        usedWords.insert(answer,at:0)
+        score=score+answer.count
+        newWord = ""
     }
-    func isOriginal(word: String) -> Bool {
-        !usedWords.contains(word)
+    func isOriginal(word:String)->Bool
+    {
+        usedWords.contains(word) ? false : true
     }
-    func isPossible(word: String) -> Bool {
-        var tempWord = rootWord
-
-        for letter in word {
-            if let pos = tempWord.firstIndex(of: letter) {
-                tempWord.remove(at: pos)
-            } else {
+    func isPossible(word:String)->Bool
+    {
+        var temp = rootWord
+        for x in word
+        {
+            if let pos = temp.firstIndex(of:x)
+            {
+                temp.remove(at:pos)
+            }
+            else
+            {
                 return false
             }
         }
         return true
     }
-    func isReal(word: String) -> Bool {
+    func isReal(word: String) -> Bool
+    {
         let checker = UITextChecker()
         let range = NSRange(location: 0, length: word.utf16.count)
         let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
 
         return misspelledRange.location == NSNotFound
     }
-    func wordError(title: String, message: String) {
+    func wordError(title: String, message: String)
+    {
         errorTitle = title
         errorMessage = message
         showingError = true
     }
+    func startGame()
+    {
+        if let gameContentUrl = Bundle.main.url(forResource:"start", withExtension:".txt")
+        {
+            if let gameContentString = try? String(contentsOf:gameContentUrl)
+            {
+                let wordArray = gameContentString.components(separatedBy:"\n")
+                rootWord=wordArray.randomElement() ?? "silkworm"
+                if score > 0
+                {
+                    score=score-5
+                }
+                return
+            }
+        }
+        
+        fatalError("Could not load start.txt from bundle.")
+    }
+    func newGame()
+    {
+        startGame()
+        score=0
+    }
+    var body: some View
+    {
+        NavigationView
+        {
+            VStack
+            {
+                TextField("Enter your word",text:$newWord,onCommit:addNewWord)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+                    .autocapitalization(/*@START_MENU_TOKEN@*/.none/*@END_MENU_TOKEN@*/)
+                Text("Your score is \(score)")
+                List(usedWords, id: \.self)
+                {
+                    Image(systemName: "\($0.count).circle")
+                    Text($0)
+                }
+            }
+            .navigationBarTitle(rootWord)
+            .navigationBarItems(leading:Button(action:newGame)
+            {
+                Text("New Game")
+            }
+            ,trailing:Button(action:startGame)
+            {
+                Text("New Word")
+            })
+            .onAppear(perform: startGame)
+            .alert(isPresented: $showingError) {
+                Alert(title: Text(errorTitle), message: Text(errorMessage), dismissButton: .default(Text("OK")))
+            }
+        }
+    }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
